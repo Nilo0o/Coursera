2f446795b24d85b106ab96fa63b454c6912865ba
NiloOofar <niloofarzangeneh@gmail.com>
2019-03-24 15:59:49 +0100

    Add files via upload

---
+++ R/cachematrix.R
@@ -0,0 +1,31 @@
+## This function will make a special list of an introduced matrix to be ready for cache storage
+
+
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+}
+
+
+##This function will give us the inversed matrix, after checking if there is any saved inveresed in the memory or not
+
+cacheSolve <- function(x, ...) {
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
+}
